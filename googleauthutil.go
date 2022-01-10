ed10013ccb7e0c702c00f64afac64ff03fa747c0
Yoshihiro Tanaka <contact@cordea.jp>
2022-01-10 13:29:57 +0900

    store/restore the token

--- googleauthutil.go
+++ googleauthutil.go
@@ -2,18 +2,26 @@ package googleauthutil
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"math/rand"
 	"net/http"
+	"os"
 )
 
-func Authorize(ctx context.Context, secret []byte, scope ...string) (*http.Client, error) {
+func Authorize(ctx context.Context, secret []byte, tokenFilename string, scope ...string) (*http.Client, error) {
 	config, err := google.ConfigFromJSON(secret, scope...)
 	if err != nil {
 		return nil, err
 	}
+
+	token, err := restoreToken(tokenFilename)
+	if err == nil {
+		return config.Client(ctx, token), nil
+	}
+
 	state := generateState()
 	url := config.AuthCodeURL(state, oauth2.AccessTypeOffline)
 	fmt.Println(url)
@@ -22,11 +30,32 @@ func Authorize(ctx context.Context, secret []byte, scope ...string) (*http.Clien
 	if _, err := fmt.Scan(&code); err != nil {
 		return nil, err
 	}
-	token, err := config.Exchange(ctx, code)
+	token, err = config.Exchange(ctx, code)
 	if err != nil {
 		return nil, err
 	}
-	return config.Client(ctx, token), nil
+	err = storeToken(tokenFilename, token)
+	return config.Client(ctx, token), err
+}
+
+func restoreToken(path string) (*oauth2.Token, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+func storeToken(path string, token *oauth2.Token) error {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewEncoder(f).Encode(token)
 }
 
 func generateState() string {
