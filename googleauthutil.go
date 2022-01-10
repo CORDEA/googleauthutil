c213e43febbe9b9e24819e5bbcc6dd781985f41f
Yoshihiro Tanaka <contact@cordea.jp>
2022-01-10 14:12:25 +0900

    avoid using printing in package

--- googleauthutil.go
+++ googleauthutil.go
@@ -6,40 +6,57 @@ import (
 	"fmt"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
 )
 
-func Authorize(ctx context.Context, secret []byte, tokenFilename string, scope ...string) (*http.Client, error) {
-	config, err := google.ConfigFromJSON(secret, scope...)
-	if err != nil {
-		return nil, err
-	}
-
-	token, err := restoreToken(tokenFilename)
-	if err == nil {
-		return config.Client(ctx, token), nil
-	}
-
-	state := generateState()
-	url := config.AuthCodeURL(state, oauth2.AccessTypeOffline)
-	fmt.Println(url)
-
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		return nil, err
-	}
-	token, err = config.Exchange(ctx, code)
-	if err != nil {
-		return nil, err
-	}
-	err = storeToken(tokenFilename, token)
-	return config.Client(ctx, token), err
+type Authorizer struct {
+	config        *oauth2.Config
+	tokenFilename string
 }
 
-func restoreToken(path string) (*oauth2.Token, error) {
-	f, err := os.Open(path)
+func NewAuthorizer(config *oauth2.Config, tokenFilename string) *Authorizer {
+	return &Authorizer{
+		config:        config,
+		tokenFilename: tokenFilename,
+	}
+}
+
+func ConfigFromJSON(filename string, scope ...string) (*oauth2.Config, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return google.ConfigFromJSON(file, scope...)
+}
+
+func (a *Authorizer) Restore(ctx context.Context) (*http.Client, error) {
+	token, err := a.restoreToken()
+	if err != nil {
+		return nil, err
+	}
+	return a.config.Client(ctx, token), nil
+}
+
+func (a *Authorizer) AuthCodeUrl() string {
+	// TODO
+	state := generateState()
+	return a.config.AuthCodeURL(state, oauth2.AccessTypeOffline)
+}
+
+func (a *Authorizer) Authorize(ctx context.Context, code string) (*http.Client, error) {
+	token, err := a.config.Exchange(ctx, code)
+	if err != nil {
+		return nil, err
+	}
+	err = a.storeToken(token)
+	return a.config.Client(ctx, token), err
+}
+
+func (a *Authorizer) restoreToken() (*oauth2.Token, error) {
+	f, err := os.Open(a.tokenFilename)
 	if err != nil {
 		return nil, err
 	}
@@ -53,8 +70,8 @@ func restoreToken(path string) (*oauth2.Token, error) {
 	return t, err
 }
 
-func storeToken(path string, token *oauth2.Token) error {
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+func (a *Authorizer) storeToken(token *oauth2.Token) error {
+	f, err := os.OpenFile(a.tokenFilename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		return err
 	}
